b6dde98ac01738cb3924172cd08d49539ec02f5a
Juan A. Romero <rukano@gmail.com>
2018-01-16 21:03:12 +0100

    on my wat to get the code shared

--- supercollider/run-nrt.sh
+++ supercollider/run-nrt.sh
@@ -1,4 +1,5 @@
 #docker create --name sc-generator --rm rukano/supercollider:nrt
 #docker cp code/sound.scd sc-generator:/supercollider/code/
 #docker start sc-generator
-docker run -it --name sc-generator --rm rukano/supercollider:nrt /bin/bash
+echo "Starting container and exeguting generate shell command"
+docker run -it --name sc-generator --rm rukano/supercollider:nrt /supercollider/generate.sh
