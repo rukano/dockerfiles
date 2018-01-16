37ca14c7f9f9390643ea76b65c64b696f842a310
Juan Romero <juan.romero@7p-group.com>
2018-01-16 14:52:37 +0100

    Score and instruments files for copying

--- supercollider/run-nrt.sh
+++ supercollider/run-nrt.sh
@@ -1,4 +1,4 @@
-docker create --name sc-generator --rm rukano/supercollider:nrt
-docker cp code/sound.scd sc-generator:/supercollider/code/
-docker start sc-generator
-# docker run -it --name sc-generator --rm rukano/supercollider:nrt /bin/bash
+#docker create --name sc-generator --rm rukano/supercollider:nrt
+#docker cp code/sound.scd sc-generator:/supercollider/code/
+#docker start sc-generator
+docker run -it --name sc-generator --rm rukano/supercollider:nrt /bin/bash
