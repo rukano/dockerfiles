d93bddffe177cab94177b3991401bf21e8690b57
Juan Romero <juan.romero@7p-group.com>
2018-01-23 16:23:12 +0100

    changed scripts for easies execution

--- supercollider/run.sh
+++ supercollider/run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name generator --volume $PWD/code:/tmp/code --volume $PWD/out:/tmp/out rukano/supercollider $1
+docker run -it --rm --name generator --volume $PWD/code:/tmp/code --volume $PWD/out:/tmp/out rukano/supercollider sclang $1
