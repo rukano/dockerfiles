cb79f25f74004c4d776c74a8cd7af368890e7b58
Juan Romero <juan.romero@7p-group.com>
2018-01-23 01:47:36 +0100

    Generator working as expected

--- supercollider/generate.sh
+++ supercollider/generate.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Executing supercollider scripts"
-cd supercollider
+cd /supercollider
 sclang startup.scd
+echo "Renaming output file to"
+echo $1.wav
+mv /rendered/out.wav /rendered/$1.wav
 echo "Finished"
